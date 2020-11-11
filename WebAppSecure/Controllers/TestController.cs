2c12cec444916079521568f699e56086b4dab12d
Jakob <jade@likeavulin.dk>
2020-11-11 20:39:27 +0100

    rsa verify data, not working

---
+++ WebAppSecure/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppSecure.Controllers
+{
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet("api/test")]
+        public string Test()
+        {
+            return "Not secure text";
+        }
+
+        [HttpGet("api/sikker")]
+        [Authorize]
+        public string Sikker()
+        {
+            return "Secure text";
+        }
+
+    }
+}
--- WebAppSecure/Startup.cs
+++ WebAppSecure/Startup.cs
@@ -14,6 +14,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Authentication.Twitter;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace WebAppSecure
 {
@@ -41,12 +45,22 @@ namespace WebAppSecure
                .AddDefaultUI()               
                .AddDefaultTokenProviders();
 
+            //var issuerSigningCertificate = new SigningIssuerCertificate();
+            RsaSecurityKey issuerSigningKey = GetIssuerSigningKey();
+
             services.AddAuthentication()
               .AddTwitter(twitterOptions =>
               {
                   twitterOptions.ConsumerKey = Configuration["Authentication:Twitter:ConsumerAPIKey"];
                   twitterOptions.ConsumerSecret = Configuration["Authentication:Twitter:ConsumerSecret"];
                   twitterOptions.RetrieveUserDetails = true;
+              })
+              .AddJwtBearer(options =>
+               {
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {               
+                        IssuerSigningKey = issuerSigningKey,
+                   };
               });
 
             services.AddCors(options =>
@@ -59,6 +73,19 @@ namespace WebAppSecure
                         builder.AllowAnyMethod();
                     });
             });
+        }
+
+        private RsaSecurityKey GetIssuerSigningKey()
+        {
+            using var rsa = new RSACryptoServiceProvider();
+
+            string publicKeyString = "4359EqA4YLmJvVxxIcwK4O7mHCXczo4ou/d2nVbOznSm5XeqtwY1EslN8uDaQGPX1xuvkAkfX9sEtWclgoId+A0GmrE/uZIu2TRbmPYws9qqwdBhNmroh5qygn4tyuyejPqNu8QshPO3uz6rHHrUeblCJW7bGWkNmrUbMZ/aoQgEpcsQurYURRKijuKI6IhFfMolVqB7GE9O72McmiLe65FH33QDiaVH4FRYsyFKPWdyj1qGl+d9l9a2NpvHd+vBVQY2c0SfSWMFQugdsKmo7gkPmZPv69RW2ErCWUPc9IgVuD+0fI+lX/K0lBWfChBkMLXdU5rUZRVGcDw+3CdCGQ==";
+
+            var p = new RSAParameters() { Modulus = Encoding.Unicode.GetBytes(publicKeyString), Exponent = Encoding.Unicode.GetBytes("AQAB") };
+
+            rsa.ImportParameters(p);
+
+            return new RsaSecurityKey(rsa);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
